32d02950a9592d81028a02b3eb2b41f7f0510f10
n0tx <rcandra91@msn.com>
2024-08-23 06:19:16 +0700

    Initialize Project Demo Spring Boot MySql DB Connection

---
+++ src/main/java/com/riki/connect_db/ConnectDbApplication.java
@@ -0,0 +1,13 @@
+package com.riki.connect_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectDbApplication.class, args);
+	}
+
+}
