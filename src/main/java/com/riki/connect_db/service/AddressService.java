9cc69a6654104784b49f495b3e1042140d4e8648
n0tx <rcandra91@msn.com>
2024-08-24 03:30:05 +0700

    address list akses data

---
+++ src/main/java/com/riki/connect_db/service/AddressService.java
@@ -0,0 +1,9 @@
+package com.riki.connect_db.service;
+
+import com.riki.connect_db.model.Address;
+
+import java.util.List;
+
+public interface AddressService {
+    List<Address> getAllAddresses();
+}
---
+++ src/main/java/com/riki/connect_db/service/impl/AddressServiceImpl.java
@@ -0,0 +1,13 @@
+package com.riki.connect_db.service.impl;
+
+import com.riki.connect_db.model.Address;
+import com.riki.connect_db.service.AddressService;
+
+import java.util.List;
+
+public class AddressServiceImpl implements AddressService {
+    @Override
+    public List<Address> getAllAddresses() {
+        return null;
+    }
+}
